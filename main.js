b31f38b59accedbfa90f6bbcde05ce380c3f68b6
Denny Setyawan Huda <dennshgit@gmail.com>
2022-09-25 01:17:20 +0700

    refactor: fix code

--- main.js
+++ main.js
@@ -1,85 +1,101 @@
-"use strict";
+const inputBook = document.getElementById("inputBook");
+const inputBookIsComplete = document.getElementById("inputBookIsComplete");
+const inputBookTitle = document.getElementById("inputBookTitle");
+const inputBookAuthor = document.getElementById("inputBookAuthor");
+const inputBookYear = document.getElementById("inputBookYear");
+const completeBookshelfList = document.getElementById("completeBookshelfList");
+const incompleteBookshelfList = document.getElementById(
+  "incompleteBookshelfList"
+);
+const searchBook = document.getElementById("searchBook");
+const searchBookTitle = document.getElementById("searchBookTitle");
 
-const submit = document.getElementById("inputBook");
-const bookTitle = document.getElementById("inputBookTitle");
-const bookAuthor = document.getElementById("inputBookAuthor");
-const bookYear = document.getElementById("inputBookYear");
-
-const isComplete = document.getElementById("inputBookIsComplete");
-
-const completeBooks = document.getElementById("completeBookshelfList");
-const incompleteBooks = document.getElementById("incompleteBookshelfList");
-
-const bookCompleteList = [];
-const bookIncompleteList = [];
-
-function generateBooksToObjects(title, author, year) {
-  return {
-    title,
-    author,
-    year,
-  };
-}
+const completeBookList = [];
+const incompleteBookList = [];
 
 function displayCompleteBooks() {
-  const display = bookCompleteList.map((book) => {
-    return `<article class="book_item">
-              <h3>Buku: ${book.title}</h3>
-              <p>Penulis: ${book.author}</p>
-              <p>Tahun: ${book.year}</p>
-            <div class="action">
-              <button class="green">Belum selesai di Baca</button>
-              <button class="red">Hapus buku</button>
-            </div>
-            </article>`;
-  });
-  completeBooks.innerHTML = display;
+  let htmlElement = "";
+  for (let index = 0; index < completeBookList.length; index++) {
+    const display = `<article class="book_item">
+                          <h3>${completeBookList[index].title}</h3>
+                          <p>Penulis: ${completeBookList[index].author}</p>
+                          <p>Tahun: ${completeBookList[index].year}</p>
+                        <div class="action">
+                          <button onclick="addToIncompleteList(${index})" onclick="" class="green">Belum selesai di Baca</button>
+                          <button onclick="removeCompleteBook(${index})" class="red">Hapus buku</button>
+                        </div>
+                      </article>`;
+    htmlElement += display;
+  }
+  completeBookshelfList.innerHTML = htmlElement;
 }
 
 function displayIncompleteBooks() {
-  const display = bookIncompleteList.map((book) => {
-    return `<article class="book_item">
-              <h3>Buku: ${book.title}</h3>
-              <p>Penulis: ${book.author}</p>
-              <p>Tahun: ${book.year}</p>
-            <div class="action">
-              <button onclick="completeReadBook()" class="green">Selesai dibaca</button>
-              <button class="red">Hapus buku</button>
-            </div>
-            </article>`;
-  });
-  incompleteBooks.innerHTML = display;
+  let htmlElement = "";
+  for (let index = 0; index < incompleteBookList.length; index++) {
+    const display = `<article class="book_item">
+                          <h3>${incompleteBookList[index].title}</h3>
+                          <p>Penulis: ${incompleteBookList[index].author}</p>
+                          <p>Tahun: ${incompleteBookList[index].year}</p>
+                        <div class="action">
+                          <button onclick="addToCompleteList(${index})" class="green">Belum selesai di Baca</button>
+                          <button onclick="removeIncompleteBook(${index})" class="red">Hapus buku</button>
+                        </div>
+                      </article>`;
+    htmlElement += display;
+  }
+  incompleteBookshelfList.innerHTML = htmlElement;
 }
 
-function addBookComplete() {
-  const newObjects = generateBooksToObjects(
-    bookTitle.value,
-    bookAuthor.value,
-    bookYear.value
-  );
-  bookCompleteList.push(newObjects);
+function addToCompleteList(index) {
+  incompleteBookList[index].isComplete = true;
+  completeBookList.push(incompleteBookList[index]);
+  incompleteBookList.splice(index, 1);
+  displayIncompleteBooks();
   displayCompleteBooks();
 }
 
-function addBookIncomplete() {
-  const newObjects = generateBooksToObjects(
-    bookTitle.value,
-    bookAuthor.value,
-    bookYear.value
-  );
-  bookIncompleteList.push(newObjects);
+function addToIncompleteList(index) {
+  completeBookList[index].isComplete = false;
+  incompleteBookList.push(completeBookList[index]);
+  completeBookList.splice(index, 1);
+  displayIncompleteBooks();
+  displayCompleteBooks();
+}
+
+function removeIncompleteBook(index) {
+  incompleteBookList.splice(index, 1);
   displayIncompleteBooks();
 }
 
-submit.addEventListener("submit", (event) => {
-  event.preventDefault();
-  if (isComplete.checked) {
-    addBookComplete();
-    console.log("work");
+function removeCompleteBook(index) {
+  completeBookList.splice(index, 1);
+  displayCompleteBooks();
+}
+
+function addBookData() {
+  if (inputBookIsComplete.checked) {
+    completeBookList.push({
+      id: +new Date(),
+      title: inputBookTitle.value,
+      author: inputBookAuthor.value,
+      year: Number(inputBookYear.value),
+      isComplete: true,
+    });
+    displayCompleteBooks();
   } else {
-    addBookIncomplete();
-    console.log("not work");
+    incompleteBookList.push({
+      id: +new Date(),
+      title: inputBookTitle.value,
+      author: inputBookAuthor.value,
+      year: Number(inputBookYear.value),
+      isComplete: false,
+    });
+    displayIncompleteBooks();
   }
-  console.log(bookCompleteList);
-  console.log(bookIncompleteList);
+}
+
+inputBook.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addBookData();
 });
